36b20d3140a5d2a2f31acdc5e12b12159dfc57b7
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-11-22 09:48:07 +0530

    default

--- cs-currency-converter/index.js
+++ cs-currency-converter/index.js
@@ -2,7 +2,7 @@ import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
 const freecurrencyapi = new Freecurrencyapi('fca_live_kZffusRGSCIrSE28Med1rckk5XIiISJSuniPpP4i');
 
-export async function currencyConverter(fromCurrency, toCurrency, units){
+export default async function currencyConverter(fromCurrency, toCurrency, units){
     const res = await freecurrencyapi.latest({
         base_currency: fromCurrency,
         currencies: toCurrency
